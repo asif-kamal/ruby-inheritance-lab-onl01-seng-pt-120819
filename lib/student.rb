ad9ff6e901388becb6f8b55265a31e8fe759dce1
Asif Kamal <asif_kamal@hotmail.com>
2020-01-22 23:38:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,9 +2,16 @@ require_relative "./vehicle.rb"
 
 class Student < User 
   
-  attr_accessor :knowledge
   
-  def initialize(knowledge)
-    @knowledge = []
+  @@knowledge = []
+  
+  def learn(knowledge)
+    @@knowledge << knowledge
+  end
+  
+  def knowledge
+    @@knowledge
+  end
+    
 
 end
